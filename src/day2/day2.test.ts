14e2b28a571c756300f5f4e8d625055fab6ea01d
ksandin <kasper.sandin@gmail.com>
2023-12-02 23:56:33 +0100

    Day 2 part 1

--- src/day2/day2.test.ts
+++ src/day2/day2.test.ts
@@ -1,10 +1,25 @@
 import { describe, expect, it } from "vitest";
 import * as part1 from "./part1";
+import * as fs from "fs";
+import * as path from "path";
 
 describe("day2", () => {
   it("part1: example", () => {
-    const result = part1.fn(exampleGameRecord);
+    const result = part1.sumOfPossibleGameIDs(exampleGameRecord, {
+      red: 12,
+      green: 13,
+      blue: 14,
+    });
     expect(result).toBe(8);
+  });
+
+  it("part2: input", () => {
+    const result = part1.sumOfPossibleGameIDs(inputText, {
+      red: 12,
+      green: 13,
+      blue: 14,
+    });
+    expect(result).toBe(1734);
   });
 });
 
@@ -13,3 +28,6 @@ Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+
+const inputFile = path.resolve(__dirname, "input.txt");
+const inputText = fs.readFileSync(inputFile, "utf-8");
--- src/day2/part1.ts
+++ src/day2/part1.ts
@@ -1,3 +1,65 @@
-export function fn(input: string): number {
-  return 0;
+export function sumOfPossibleGameIDs(
+  input: string,
+  availableCubes: CubeCounts
+): number {
+  const games = parseGames(input);
+  const possibleGames = games.filter((game) =>
+    game.cubes.every((set) => isSetPossible(set, availableCubes))
+  );
+  const sumOfIDs = possibleGames.reduce((sum, game) => sum + game.id, 0);
+  return sumOfIDs;
 }
+
+function parseGames(input: string): Game[] {
+  const lines = input.split(/[\n\r]+/);
+  return lines.map(parseGame);
+}
+
+function parseGame(input: string): Game {
+  const result = /^game\s+(\d+):(.*)$/i.exec(input);
+  if (!result) {
+    throw new Error("Could not parse game");
+  }
+  const [, idString, setsText] = result;
+  return {
+    id: Number(idString),
+    cubes: parseCubeSets(setsText),
+  };
+}
+
+function parseCubeSets(setsText: string): CubeSet[] {
+  return setsText.split(";").map((setText) =>
+    Object.fromEntries(
+      setText.split(",").map((cubeText) => {
+        const result = /^(\d+)\s+(\w+)$/i.exec(cubeText.trim());
+        if (!result) {
+          throw new Error("Could not parse cube");
+        }
+        const [, countText, colorName] = result;
+        return [colorName as Color, Number(countText)] as const;
+      })
+    )
+  );
+}
+
+function isSetPossible(
+  gameCubes: Partial<CubeCounts>,
+  availableCubes: CubeCounts
+): boolean {
+  for (const [color, count] of Object.entries(gameCubes)) {
+    const maxCount = availableCubes[color as Color];
+    if (count > maxCount) {
+      return false;
+    }
+  }
+  return true;
+}
+
+interface Game {
+  id: number;
+  cubes: CubeSet[];
+}
+
+type Color = "red" | "blue" | "green";
+type CubeSet = Partial<CubeCounts>;
+type CubeCounts = Record<Color, number>;
