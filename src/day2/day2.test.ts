5557a456d09d42f5dd1500a2cccfd0041287cb37
ksandin <kasper.sandin@gmail.com>
2023-12-02 23:20:34 +0100

    Set up day2 example test boundaries

---
+++ src/day2/day2.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from "vitest";
+import * as part1 from "./part1";
+
+describe("day2", () => {
+  it("part1: example", () => {
+    const result = part1.fn(exampleGameRecord);
+    expect(result).toBe(8);
+  });
+});
+
+const exampleGameRecord = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
